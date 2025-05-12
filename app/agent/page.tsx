a5667dd4a93380892aa35b98120c7957691e830c
tan-c <tanchen2014@gmail.com>
2025-05-12 18:25:12 +0900

    adding more mock

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -1,12 +1,46 @@
 "use client";
 
+import { users } from "@/data/users";
 import { useState } from "react";
 
 export default function Agent() {
 
-    return (
-        <div>
-            <h1>Agent</h1>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-2 p-4 py-8">
+      <h1 className="text-2xl font-bold">Agent Page</h1>
+
+      <hr />
+
+      <div>
+        <h2>Code Usage</h2>
+        // TO BE FILLED
+      </div>
+
+      <hr />
+
+      <div>
+        <h2>User List</h2>
+        <ul>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </ul>
+      </div>
+    </div>
+  )
 }   
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -3,9 +3,19 @@
 import { useState } from "react";
 
 export default function User() {
-    return (
-        <div>
-            <h1>User</h1>
-        </div>
-    )
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="flex flex-col gap-2 p-4 py-8">
+      <h1 className="text-2xl font-bold">User Page</h1>
+
+      <hr />
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} className="border-2 border-gray-300 rounded-md p-2" />
+
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={() => {
+        // TO BE FILLED
+        console.log(input);
+      }}>Use Code</button>
+    </div>
+  )
 }   
